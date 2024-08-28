5af24c2297f0e1ed0baaed824569058abc8b2a64
Stefan Sabev <stevenbg91@gmail.com>
2024-08-28 18:26:10 +0200

    Create index.ts

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const sidebarLinks = [
+    {
+        route: "/profile",
+        label: "Profile",
+        imageURL: "/icons/microphone.svg"
+    },
+    {
+        route: "/home",
+        label: "Home",
+        imageURL: "/icons/home.svg"
+    }
+];
